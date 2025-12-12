ae8b7be1376d6fab9664d678a267ae7928e064ac
Cherif Chikh <chikhcherif4@gmail.com>
2025-12-12 11:18:08 +0100

    Initial commit

---
+++ WEB/src/myParam.inc.php
@@ -0,0 +1,10 @@
+<?php
+define("HOST", "127.0.0.1");
+<<<<<<< HEAD
+define("USER", "");
+define("PASS", "");
+=======
+define("USER", "cchikh");
+define("PASS", "Momopop51@");
+>>>>>>> 1b9161d (bug redirection)
+?>
